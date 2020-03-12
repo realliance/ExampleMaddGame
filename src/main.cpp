6990930b471d6f092bfaf301e2a5a59b4ad4a8b7
Alice Hartley <alicehartleyhartley@gmail.com>
2020-03-12 05:55:46 -0400

    Added Delete entity example

--- src/main.cpp
+++ src/main.cpp
@@ -24,24 +24,24 @@ int main(){
 
     Madd::GetInstance().InitSystems();
 
-    KeyboardEventComponent exitCB = KeyboardEventComponent{};
-    exitCB.callback = [](Component* c, WindowComponent* window, int key, int action){Madd::GetInstance().Close();};
-    exitCB.code = KEY_ESCAPE;
+    KeyboardEventComponent exitEvent = KeyboardEventComponent{};
+    exitEvent.callback = [](Component* c, WindowComponent* window, int key, int action){
+      Madd::GetInstance().Close();
+    };
+    exitEvent.code = KEY_ESCAPE;
     GameCamera* gameCamera = new GameCamera();
     WindowComponent window{};
     window.height = 9*64;
     window.width = 16*64;
     window.title = "ExampleMaddGame";
     window.cameras.push_back(&gameCamera->camera.camera);
-
-    Entity mainWindow{&exitCB,&window};
-    Madd::GetInstance().CreateEntity(&mainWindow);
+    Madd::GetInstance().CreateEntity({&exitEvent,&window});
 
     gameCamera->camera.movementSpeed = 16.f;
 
     WorldComponent world{};
     world.stretch = {256, 128, 256};
-    world.size = {256,256};
+    world.size = {2,2};
     world.seed = "test_seed";
     Madd::GetInstance().RegisterComponent(&world);
 
@@ -61,8 +61,39 @@ int main(){
       glm::vec4(0.3f, 0.4f, 0.6f, 1.f) //Shade
     );
     
-    Entity fruits{&mesh,&shader,&r};
-    Madd::GetInstance().CreateEntity(&fruits);
+    Entity fruits = {&mesh,&shader,&r};
+    EntityID fruitID = Madd::GetInstance().CreateEntity({&mesh,&shader,&r});
+        
+    KeyboardEventComponent deleteFruitEvent = KeyboardEventComponent{};
+    KeyboardEventComponent createFruitEvent = KeyboardEventComponent{};
+
+    bool fruitdeleted = false;
+    deleteFruitEvent.callback = [&fruitdeleted,&fruitID](Component* c, WindowComponent* window, int key, int action){
+      if(action != KEY_PRESS){
+        return;
+      }
+      if(Madd::GetInstance().DeleteEntity(fruitID)){
+        fruitdeleted = true;
+        std::cout << "Fruit Deleted" << std::endl;
+      }else{
+        std::cout << "Fruit already Deleted or Error occured." << std::endl;
+      }
+    };
+    deleteFruitEvent.code = KEY_0;
+    createFruitEvent.callback = [&fruitdeleted,&fruitID,&fruits](Component* c, WindowComponent* window, int key, int action){
+      if(action != KEY_PRESS){
+        return;
+      }
+      if(fruitdeleted){
+        fruitID = Madd::GetInstance().CreateEntity(fruits);
+        fruitdeleted = false;
+        std::cout << "Fruit Created" << std::endl;
+      }else{
+        std::cout << "Fruit already created" << std::endl;
+      }
+    };
+    createFruitEvent.code = KEY_9;
+    Madd::GetInstance().CreateEntity({&deleteFruitEvent,&createFruitEvent});
 
     Madd::GetInstance().Run();
     Madd::GetInstance().Deinit();
